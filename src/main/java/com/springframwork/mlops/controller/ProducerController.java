7a9daf16502e1763b577cb09464a98cfee6afe72
Osvaldo Ignacio Rios Zambrano <osvaldorioz@yahoo.com.mx>
2024-04-03 16:40:11 -0600

    Add files via upload

---
+++ src/main/java/com/springframwork/mlops/controller/ProducerController.java
@@ -0,0 +1,29 @@
+package com.springframwork.mlops.controller;
+
+import com.springframwork.mlops.domain.User;
+import com.springframwork.mlops.service.RabbitMqSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/v1/")
+public class ProducerController {
+
+
+    private RabbitMqSender rabbitMqSender;
+
+    @Autowired
+    public ProducerController(RabbitMqSender rabbitMqSender) {
+        this.rabbitMqSender = rabbitMqSender;
+    }
+
+    @Value("${app.message}")
+    private String message;
+
+    @PostMapping(value = "user")
+    public String publishUserDetails(@RequestBody User user) {
+        rabbitMqSender.send(user);
+        return message;
+    }
+}
